f622060878f37d5784cfc10feff4aa1cbbeb992d
Anonymous Shrimp <yumiswep@gmail.com>
2022-05-06 02:31:01 -0700

    code reformatting

---
+++ Assets/Level/Camera/CameraBob.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBob : MonoBehaviour
+{
+    public bool cameraBob = true;
+    float originalY;
+    public float speed;
+    public float magnitute;
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalY = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(cameraBob)
+        {
+            float offsetY = magnitute * Mathf.Sin(speed * Time.time);
+
+            transform.position = new Vector3(transform.position.x, offsetY + originalY, transform.position.z);
+        }
+    }
+}
---
+++ Assets/Player/Movement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    CharacterController characterController;
+
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    public float rotSpeed = 10;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (characterController.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection = Camera.main.transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            
+        }
+        
+        moveDirection.y -= gravity * Time.deltaTime;
+        
+        characterController.Move(moveDirection * Time.deltaTime);
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        if (v != 0 || h != 0)
+        {
+            Vector3 fwd = new Vector3(h, 0, v);
+            fwd = Camera.main.transform.TransformDirection(fwd);
+            fwd.y = 0;
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(fwd), rotSpeed * Time.deltaTime);
+        }
+    }
+}
